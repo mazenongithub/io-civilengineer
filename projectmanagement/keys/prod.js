d4d6bbdf43717a2324b4a2c080f31d4e7d1b56c5
MazenonCloud9 <mazen@egeotechnical.com>
2019-10-16 05:28:42 +0000

    updated keys prod

--- projectmanagement/keys/prod.js
+++ projectmanagement/keys/prod.js
@@ -2,5 +2,5 @@ module.exports = {
     sessionSecret: process.env.pmSessionSecret,
     grantAuthorization: process.env.pmAuthorization,
     clientAPI: process.env.pmclientAPI,
-    secretAPI:process.env.pmsecretAPI
+    secretAPI: process.env.pmSecretAPI
 }
