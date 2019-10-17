34b5463dcb4d47eda016e2e7524ed10032936938
MazenonCloud9 <mazen@egeotechnical.com>
2019-10-17 20:22:33 +0000

    added access control

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ app.use(session({
     }
 
 ));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 require('./projectmanagement')(app);
 require('./geotechnical')(app);
 app.get('/', (req, res) => {
