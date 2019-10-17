bb10aa224b3163038f9f356e0be5041c04e8c0c3
MazenonCloud9 <mazen@egeotechnical.com>
2019-10-17 20:34:48 +0000

    removed cors

--- projectmanagement/index.js
+++ projectmanagement/index.js
@@ -1,7 +1,11 @@
-const cors = require('cors')
+const keys = require('./keys')
 module.exports = app => {
-    app.use(cors())
-
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", keys.clientAPI);
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Credentials", "true")
+        next();
+    });
 
     require('./routes/paymentrouter')(app);
     require('./routes/project')(app);
