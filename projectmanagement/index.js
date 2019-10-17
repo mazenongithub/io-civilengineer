603afa48598c37d0127c49a7c76fc11c0ec7ee61
MazenonCloud9 <mazen@egeotechnical.com>
2019-10-17 20:41:19 +0000

    update header

--- projectmanagement/index.js
+++ projectmanagement/index.js
@@ -2,6 +2,7 @@ const keys = require('./keys')
 module.exports = app => {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", keys.clientAPI);
+        res.header("Access-Control-Allow-Origin", "https://projectmanagement.civilengineer.io")
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         res.header("Access-Control-Allow-Credentials", "true")
         next();
