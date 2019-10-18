5353275c6ffc106125e673cc8dc0512b8df9d0fb
MazenonCloud9 <mazen@egeotechnical.com>
2019-10-18 00:51:10 +0000

    try this

--- projectmanagement/index.js
+++ projectmanagement/index.js
@@ -2,8 +2,8 @@ const keys = require('./keys')
 module.exports = app => {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", keys.clientAPI);
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
         res.header("Access-Control-Allow-Credentials", "true")
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
 
