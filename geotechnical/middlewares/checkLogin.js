6e441cc405df28afd528f334fb57f03f592b6fe2
MazenonCloud9 <mazen@egeotechnical.com>
2019-10-19 15:35:20 +0000

    added cors with options

--- geotechnical/middlewares/checkLogin.js
+++ geotechnical/middlewares/checkLogin.js
@@ -1,15 +1,18 @@
 module.exports = (req, res, next) => {
-    
-        if(req.params.clientid === "gus") {
-      return next();    
-    } else if (req.hasOwnProperty("session")) {
+
+    if (req.params.clientid === "gus" || req.params.clientid === "mazen") {
+        return next();
+    }
+    else if (req.hasOwnProperty("session")) {
         if (req.session.hasOwnProperty("user")) {
             if (req.session.user.hasOwnProperty("clientid")) {
-                if(req.session.user.clientid === "mazen") {
-                  return next();
-                }  else if (req.params.clientid === req.session.user.clientid) {
-                   return next();
-                }  else {
+                if (req.session.user.clientid === "mazen") {
+                    return next();
+                }
+                else if (req.params.clientid === req.session.user.clientid) {
+                    return next();
+                }
+                else {
                     return res.status(401).send({ errorMessage: ' You must be logged in view this ' });
                 }
 
--- projectmanagement/index.js
+++ projectmanagement/index.js
@@ -1,11 +1,11 @@
 const keys = require('./keys')
 module.exports = app => {
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", keys.clientAPI);
-        res.header("Access-Control-Allow-Credentials", "true")
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
+    //app.use(function(req, res, next) {
+    //    res.header("Access-Control-Allow-Origin", keys.clientAPI);
+    //    res.header("Access-Control-Allow-Credentials", "true")
+    //    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //    next();
+    //});
 
     require('./routes/paymentrouter')(app);
     require('./routes/project')(app);
