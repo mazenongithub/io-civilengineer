31837d65992ac01ea16eea54c144910231326234
MazenonCloud9 <mazen@egeotechnical.com>
2019-10-31 17:34:31 +0000

    update to prod file

--- keys/prod.js
+++ keys/prod.js
@@ -1,3 +1,11 @@
 module.exports = {
-    RENTMEROOM: process.env.RENTMEROOM
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    GOOGLEID: process.env.GOOGLEID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    YAHOOID: process.env.YAHOOID,
+    YAHOO_SECRET: process.env.YAHOO_SECRET,
+    STRIPE_CONNECT: process.env.STRIPE_CONNECT,
+    STRIPE_SECRET: process.env.STRIPE_SECRET,
+    SERVER_API: process.env.SERVER_API
 }
