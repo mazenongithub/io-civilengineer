2773a9ce86374162848e886085ed759083b1d438
MazenonCloud9 <mazen@egeotechnical.com>
2019-11-22 22:53:57 +0000

    added native client register

--- projectmanagement/routes/authroutes/native.js
+++ projectmanagement/routes/authroutes/native.js
@@ -3,10 +3,34 @@ const serverkeys = require('../../../keys');
 const request = require("request");
 const parser = require('xml2json');
 const updateAllProjects = require('../../functions/updateallprojects')
+const checkLogin = require('../../functions/checkLogin');
 module.exports = app => {
+
+    app.post('/projectmanagement/:providerid/nativeclientregister', checkLogin, (req, res) => {
+
+        request.post({
+                url: `${keys.secretAPI}/updateuserprofile.php`,
+                form: req.body,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Permission': `${keys.grantAuthorization}`
+                }
+            },
+            function(err, httpResponse, body) {
+                if (!err) {
+                    var json = parser.toJson(body);
+                    var parsedjson = JSON.parse(json)
+                    let response = parsedjson.response;
+                    req.session.user = { providerid: response.providerid }
+                    res.send(response)
+                }
+
+
+            }) // end request
+
+    })
     app.post('/projectmanagement/nativelogin', (req, res) => {
         //let values = { client: 'apple', clientid: 'udderedssdd8444.u', firstname: 'Mazen', lastname: 'Khenaisser', emailaddress: 'mazen@civilengineer.io' }
-        console.log(req.body)
         request.post({
                 url: `${keys.secretAPI}/loginclient.php`,
                 form: req.body,
@@ -21,7 +45,6 @@ module.exports = app => {
                     var parsedjson = JSON.parse(json)
                     let response = parsedjson.response;
                     response = updateAllProjects(response);
-                    console.log(response)
                     res.send(response)
 
                 }
