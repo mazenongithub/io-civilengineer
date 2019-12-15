9770b7da5092ceba0d50f32d4516090e7d9a9ee7
MazenonCloud9 <mazen@egeotechnical.com>
2019-12-15 23:25:29 +0000

    updated cors for petitions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const cors = {
-    origin: ["http://rentmeroom.civilengineer.io", "http://geotechnical.civilengineer.io", "http://pm.civilengineer.io", "http://localhost:3000"]
+    origin: ["http://rentmeroom.civilengineer.io", "http://geotechnical.civilengineer.io", "http://pm.civilengineer.io", "http://localhost:3000", "http://petitions.civilengineer.io"]
 }
 
 
@@ -19,7 +19,7 @@ app.all('*', function(req, res, next) {
     let origin = req.headers.origin;
     if (cors.origin.indexOf(origin) >= 0) {
         res.header("Access-Control-Allow-Origin", origin);
-        console.log(origin)
+
     }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true")
@@ -39,7 +39,7 @@ require('./projectmanagement')(app);
 require('./geotechnical')(app);
 require('./petitions')(app);
 app.get('/', (req, res) => {
-    res.send({ response: "Hello New App" })
+    res.send({ response: "api.civilengineer.io" })
 })
 
 app.listen(process.env.PORT);
