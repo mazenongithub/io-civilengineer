c166e71ad5335b7fa59cffbdc92dcb3543d18c55
MazenonCloud9 <mazen@egeotechnical.com>
2021-03-16 23:25:08 +0000

    updated AWS prod keys

--- appbaseddriver/functions/checkuser.js
+++ appbaseddriver/functions/checkuser.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
+    
     if (req.hasOwnProperty("session")) {
 
         if (req.session.hasOwnProperty("appbaseddriver")) {
--- keys/prod.js
+++ keys/prod.js
@@ -3,5 +3,7 @@ module.exports = {
     STRIPE_CONNECT: process.env.STRIPE_CONNECT,
     STRIPE_SECRET: process.env.STRIPE_SECRET,
     secure: true,
-    MONGODB: process.env.MONGODB
+    MONGODB: process.env.MONGODB,
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
 }
