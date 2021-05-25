cc2ecc05cd2d6fe75b476771fb303f4e551844e6
MazenonCloud9 <mazen@egeotechnical.com>
2021-05-25 22:13:28 +0000

    idk

--- petitions/functions/validateuser.js
+++ petitions/functions/validateuser.js
@@ -1,8 +1,9 @@
 module.exports = (req, res, next) => {
 
     if (req.session) {
-        console.log(req.session)
+
         if (req.session.petitions) {
+            console.log(req.session.petitions, req.params.profile)
             if (req.session.petitions.profile === req.params.profile) {
                 res.send({ valid: ` Destination Profile matches session variable ` })
             }
