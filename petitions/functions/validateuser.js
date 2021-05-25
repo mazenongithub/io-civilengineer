8728431fa54780c0c67606985035f3acef07c5bb
MazenonCloud9 <mazen@egeotechnical.com>
2021-05-25 22:29:07 +0000

    idk

--- petitions/functions/validateuser.js
+++ petitions/functions/validateuser.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     console.log("validate user")
 
     if (req.session) {
-        console.log("session")
+        console.log("session", console.log(req.params.profile))
         if (req.session.petitions) {
             console.log("petitions")
             console.log(req.session.petitions, req.params.profile)
