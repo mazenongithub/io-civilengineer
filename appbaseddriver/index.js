32b645aae566b13f1b6242102a0935ae7336d52b
MazenonCloud9 <mazen@egeotechnical.com>
2021-06-22 01:24:38 +0000

    restored

--- appbaseddriver/index.js
+++ appbaseddriver/index.js
@@ -442,7 +442,7 @@ module.exports = app => {
 
                     if (succ.driverid) {
                         const appbaseddriver = new AppBasedDriver();
-                        console.log("success", succ)
+                        console.log("success", succ.driver)
 
                         const json = appbaseddriver.annualReport(succ, year)
                         const response = js2xmlparser.parse("driver", json)
